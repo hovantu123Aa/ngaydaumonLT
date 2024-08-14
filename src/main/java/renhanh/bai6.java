c4a51e0baf9cd451079fa7ade24a02f89c1e37bf
ADMIN <ADMIN@PC313>
2024-08-14 16:48:32 +0700

    phien ban 3

--- src/main/java/renhanh/bai6.java
+++ src/main/java/renhanh/bai6.java
@@ -4,10 +4,40 @@
  */
 package renhanh;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ADMIN
  */
 public class bai6 {
-    
+    public static void main(String[] args) {
+        int a;
+        int b;
+        Scanner sc=new Scanner(System.in);
+        System.out.println(" vui long nhap so nguyen thu nhat");
+        System.out.println(" vui long nhap so nguyen thu hai    ");
+        a=sc.nextInt();
+        b=sc.nextInt();
+        System.out.println("vui long nhap phép toán +,-,*,/:");
+        int kq=0;
+        switch(kq){
+             case '+':
+            System.out.print("ket qua cua phep cong"+(a+b));
+            break;
+            case '-':
+            System.out.print("ket qua cua phep tru " +(a-b));
+              case '*':
+            System.out.print("ket qua cua phep nhan" +(a*b));
+            case '/':
+            if(b!=0){
+                System.out.println("ket qua cua phep nha"+((double)b/a));
+            }else{
+              System.out.print("không chia het :");
+            }
+            break;
+            default: 
+                break;
+        }    
+    }
 }
